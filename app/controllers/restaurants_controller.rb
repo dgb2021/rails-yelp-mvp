ae4704b1c7a8e49796e9c0d5f12c1a6c0d0d0578
Doug Barr <douglas.barr01@gmail.com>
2021-05-26 15:39:14 +0100

    added routes/controllers and blank html pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,45 @@
 class RestaurantsController < ApplicationController
+  
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+      if @restaurant.save
+        redirect_to restaurant_path(@restaurant), notice: "Successfully saved"
+      else
+        render :new
+      end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path, notice: "Successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @restaurant = @restaurants.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  resources :restaurants do
+    resources :reviews, only: [ :index, :new, :show, :create ]
+    end
+    resources :comments, only: [ :show, :edit, :update, :destroy] 
+  end
+  
+  # # A visitor can see the list of all restaurants.
+  # get "restaurants", to: "restaurants#index"
+  # # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # get "restaurant/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # get "restaurants/:id", to: "restaurants#show"
+  # # A visitor can add a new review to a restaurant  
+  # # NESTED RESOURCE
+  # get "restaurants/:id/review/new", to: "restaurants#new"
+  # post
+  
